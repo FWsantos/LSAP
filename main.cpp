4b08737c88c78e894c8096e12c6c5ad802f4e353
blade_baiano <fwsantos23@gmail.com>
2022-08-25 10:59:41 -0300

    Add giignore

---
+++ main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+
+int main(int argc, char const *argv[])
+{
+    std::cout << "teste\n";
+    return 0;
+}
---
+++ preprocessor.cpp
@@ -0,0 +1,19 @@
+#include <vector>
+
+class preprocessor
+{
+private:
+    /* data */
+public:
+    preprocessor(std::vector<int>);
+    ~preprocessor();
+};
+
+preprocessor::preprocessor()
+{
+    
+}
+
+preprocessor::~preprocessor()
+{
+}
