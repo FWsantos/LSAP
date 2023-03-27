b7c555436ba32b0900e879d95a77a91f3982478d
FWsantos <fwsantos23@gmail.com>
2023-03-27 17:11:22 -0300

    add preprcessing

--- include/LSAP.h
+++ include/LSAP.h
@@ -1,3 +1,16 @@
-namespace LSAP{
-    void printTeste();
-}
+#include <set>
+#include <vector>
+
+/* Matrix type */
+typedef std::vector<std::vector<int>> matrix;
+
+class LSAP {
+public:
+    static void printTeste();
+    static void basic_preprocessing(matrix C, matrix &X, std::vector<int> &row, int n);
+    static void procedure_alternate(matrix C, std::set<int> V, int k);
+
+private:
+    static void row_reduction(matrix C, std::vector<int> &u, int n);
+    static void column_reduction(matrix C, std::vector<int> u, std::vector<int> &v, int n);
+};
