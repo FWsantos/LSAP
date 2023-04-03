7a5580be93ba1804d5d309b0e5ec05a46a15df2b
FWsantos <fwsantos23@gmail.com>
2023-04-03 20:38:56 -0300

    Adds procedure_alternate method.

--- include/LSAP.h
+++ include/LSAP.h
@@ -7,10 +7,33 @@ typedef std::vector<std::vector<int>> matrix;
 class LSAP {
 public:
     static void printTeste();
-    static void basic_preprocessing(matrix C, matrix &X, std::vector<int> &row, int n);
-    static void procedure_alternate(matrix C, std::set<int> V, int k);
+    static void basic_preprocessing(
+        matrix C,
+        matrix &X,
+        std::vector<int> &row,
+        int n
+    );
+    static void procedure_alternate(
+        matrix C,
+        std::set<int> V,
+        std::vector<int> u,
+        std::vector<int> v,
+        std::vector<int> row,
+        int &sink,
+        int k,
+        int n
+    );
 
 private:
-    static void row_reduction(matrix C, std::vector<int> &u, int n);
-    static void column_reduction(matrix C, std::vector<int> u, std::vector<int> &v, int n);
+    static void row_reduction(
+        matrix C,
+        std::vector<int> &u,
+        int n
+    );
+    static void column_reduction(
+        matrix C,
+        std::vector<int> u,
+        std::vector<int> &v,
+        int n
+    );
 };
--- src/LSAP.cpp
+++ src/LSAP.cpp
@@ -49,21 +49,50 @@ void LSAP::basic_preprocessing(matrix C, matrix &X, std::vector<int> &row, int n
     }
 }
 
-// void procedure_alternate(matrix C, std::set<int> V, int k) {
-//     std::set<int> SU, LV, SV, V_dif_LV;
-//     std::vector<int> pred;
-//     bool fail = false;
-//     int sink = 0;
-//     int i = k; 
+void procedure_alternate(
+    matrix C,
+    std::set<int> V,
+    std::vector<int> u,
+    std::vector<int> v,
+    std::vector<int> row,
+    int &sink,
+    int k,
+    int n
+) {
+    std::set<int> SU, LV, SV, V_dif_LV, LV_dif_SV;
+    std::vector<int> pred;
+    bool fail = false;
+    // int sink = 0;
+    int i = k;
 
-//     while(fail == false && sink == 0) {
-//         SU.insert(i);
-//         std::set_difference(V.begin(), V.end(), LV.begin(), LV.end(), std::back_inserter(V_dif_LV));
-//         for (int j = 0; j < static_cast<int>(V_dif_LV.size()); j++) {
-                        
-//         }
-        
-//     }
-// }
+    while(fail == false && sink == 0) {
+        SU.insert(i);
+
+        std::set_difference(V.begin(), V.end(), LV.begin(), LV.end(), std::inserter(V_dif_LV, V_dif_LV.begin()));
+
+        for (
+            int j = 0;
+            j < static_cast<int>(V_dif_LV.size() || C[i][j] - u[i] - v[j] == 0);
+            j++
+        ) {
+            pred[j] = i;
+            LV.insert(j);
+        }
+
+        std::set_difference(LV.begin(), LV.end(), SV.begin(), SV.end(), std::inserter(LV_dif_SV, LV_dif_SV.begin()));
+        if (LV_dif_SV.empty()) {
+            fail = true;
+        } else {
+            std::for_each(LV_dif_SV.begin(), LV_dif_SV.end(), [&](const int& j){
+                SV.insert(j);
+                if(row[j] == 0){
+                    sink = j;
+                }else{
+                    i = row[j];
+                }
+            });
+        }
+    }
+}
 
 
